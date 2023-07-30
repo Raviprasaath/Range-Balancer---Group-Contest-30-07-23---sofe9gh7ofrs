2889121eca733b888b14f454af6dddbeff45f422
Raviprasaath Ramesh <117162868+Raviprasaath@users.noreply.github.com>
2023-07-30 18:15:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,44 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  const [maxSum, setMaxSum] = useState(50);
+  const [rangeA, setRangeA] = useState(0);
+  const [rangeB, setRangeB] = useState(50);
+
+  const handleMaxSumChange = (event) => {
+    setMaxSum(parseInt(event.target.value));
+  };
+
+  const handleRangeAChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    setRangeA(Math.min(newValue, maxSum - rangeB));
+  };
+
+  const handleRangeBChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    setRangeB(Math.min(newValue, maxSum - rangeA));
+  };
+
+  return (
+    
+    <>
+       <div id="max-sum-holder">
+          <label htmlFor="max-sum">Max Sum:</label>
+          <input id="max-sum" type="number" value={maxSum} onChange={handleMaxSumChange}/>
+        </div>
+        <div id="range-b-holder">
+          <label htmlFor="range-b">Range B:</label>
+          <input id="range-b" type="range" value={rangeB} max={maxSum - rangeA} onChange={handleRangeBChange}/>
+          <div id="range-b-value">{rangeB}</div>
+        </div>
+        <div id="sum">
+          Sum: {rangeA + rangeB}
+        </div>
+    </div>
+            
+    </>
+  )
+    
   )
 }
 
