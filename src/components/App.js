5e53822af4642db5bb8779188226e4fc0de88792
Raviprasaath Ramesh <117162868+Raviprasaath@users.noreply.github.com>
2023-07-30 18:26:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
+
 const App = () => {
   const [maxSum, setMaxSum] = useState(50);
   const [rangeA, setRangeA] = useState(0);
@@ -21,27 +21,46 @@ const App = () => {
   };
 
   return (
-    
-    <>
-       <div id="max-sum-holder">
-          <label htmlFor="max-sum">Max Sum:</label>
-          <input id="max-sum" type="number" value={maxSum} onChange={handleMaxSumChange}/>
-        </div>
-        <div id="range-b-holder">
-          <label htmlFor="range-b">Range B:</label>
-          <input id="range-b" type="range" value={rangeB} max={maxSum - rangeA} onChange={handleRangeBChange}/>
-          <div id="range-b-value">{rangeB}</div>
-        </div>
-        <div id="sum">
-          Sum: {rangeA + rangeB}
-        </div>
-    </div>
-            
-    </>
-  )
-    
-  )
-}
+    <div>
+      <div id="max-sum-holder">
+        <label htmlFor="max-sum">Max Sum:</label>
+        <input
+          id="max-sum"
+          type="number"
+          value={maxSum}
+          onChange={handleMaxSumChange}
+        />
+      </div>
 
+      <div id="range-a-holder">
+        <label htmlFor="range-a">Range A:</label>
+        <input
+          id="range-a"
+          type="range"
+          value={rangeA}
+          max={maxSum - rangeB}
+          onChange={handleRangeAChange}
+        />
+        <div id="range-a-value">{rangeA}</div>
+      </div>
+
+      <div id="range-b-holder">
+        <label htmlFor="range-b">Range B:</label>
+        <input
+          id="range-b"
+          type="range"
+          value={rangeB}
+          max={maxSum - rangeA}
+          onChange={handleRangeBChange}
+        />
+        <div id="range-b-value">{rangeB}</div>
+      </div>
+
+      <div id="sum">
+        Sum: {rangeA + rangeB}
+      </div>
+    </div>
+  );
+};
 
 export default App;
